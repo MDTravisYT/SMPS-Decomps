d87edc43e7adabc80b3becb726322b626499e8c3
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-03-01 18:38:16 -0500

    proper voices in crackers 0A

--- Sonic Crackers/Sound0A - 0F.asm
+++ Sonic Crackers/Sound0A - 0F.asm
@@ -12,3 +12,20 @@ Sound0A_FM5:
 
 ; Song seems to not use any FM voices
 Sound0A_Voices:
+;	Voice $00
+;	$3C
+;	$01, $01, $01, $01, 	$1F, $1F, $1F, $1F, 	$00, $00, $00, $00
+;	$00, $00, $00, $00, 	$0F, $0F, $0F, $0F, 	$00, $80, $00, $80
+	smpsVcAlgorithm     $04
+	smpsVcFeedback      $07
+	smpsVcUnusedBits    $00
+	smpsVcDetune        $00, $00, $00, $00
+	smpsVcCoarseFreq    $01, $01, $01, $01
+	smpsVcRateScale     $00, $00, $00, $00
+	smpsVcAttackRate    $1F, $1F, $1F, $1F
+	smpsVcAmpMod        $00, $00, $00, $00
+	smpsVcDecayRate1    $00, $00, $00, $00
+	smpsVcDecayRate2    $00, $00, $00, $00
+	smpsVcDecayLevel    $00, $00, $00, $00
+	smpsVcReleaseRate   $0F, $0F, $0F, $0F
+	smpsVcTotalLevel    $00, $00, $00, $00
