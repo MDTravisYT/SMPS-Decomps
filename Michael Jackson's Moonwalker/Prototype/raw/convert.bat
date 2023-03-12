81725d3b16b576a370a340427252f2d7fd4c4912
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-03-11 20:33:55 -0500

    moonwalker proto sfx

---
+++ Michael Jackson's Moonwalker/Prototype/raw/convert.bat
@@ -0,0 +1,55 @@
+echo	Converting Section 1...
+smps2asm -v 1 -s A0.BIN A0.ASM MJ_SND_A0
+smps2asm -v 1 -s A1.BIN A1.ASM MJ_SND_A1
+smps2asm -v 1 -s A2.BIN A2.ASM MJ_SND_A2
+smps2asm -v 1 -s A3.BIN A3.ASM MJ_SND_A3
+smps2asm -v 1 -s A4.BIN A4.ASM MJ_SND_A4
+smps2asm -v 1 -s A5.BIN A5.ASM MJ_SND_A5
+smps2asm -v 1 -s A6.BIN A6.ASM MJ_SND_A6
+smps2asm -v 1 -s A7.BIN A7.ASM MJ_SND_A7
+smps2asm -v 1 -s A8.BIN A8.ASM MJ_SND_A8
+smps2asm -v 1 -s A9.BIN A9.ASM MJ_SND_A9
+smps2asm -v 1 -s AA.BIN AA.ASM MJ_SND_AA
+smps2asm -v 1 -s AB.BIN AB.ASM MJ_SND_AB
+smps2asm -v 1 -s AC.BIN AC.ASM MJ_SND_AC
+smps2asm -v 1 -s AD.BIN AD.ASM MJ_SND_AD
+smps2asm -v 1 -s AE.BIN AE.ASM MJ_SND_AE
+smps2asm -v 1 -s AF.BIN AF.ASM MJ_SND_AF
+echo	Converting Section 2...
+smps2asm -v 1 -s B0.BIN B0.ASM MJ_SND_B0
+smps2asm -v 1 -s B1.BIN B1.ASM MJ_SND_B1
+smps2asm -v 1 -s B2.BIN B2.ASM MJ_SND_B2
+smps2asm -v 1 -s B3.BIN B3.ASM MJ_SND_B3
+smps2asm -v 1 -s B4.BIN B4.ASM MJ_SND_B4
+smps2asm -v 1 -s B5.BIN B5.ASM MJ_SND_B5
+smps2asm -v 1 -s B6.BIN B6.ASM MJ_SND_B6
+smps2asm -v 1 -s B7.BIN B7.ASM MJ_SND_B7
+smps2asm -v 1 -s B8.BIN B8.ASM MJ_SND_B8
+smps2asm -v 1 -s B9.BIN B9.ASM MJ_SND_B9
+smps2asm -v 1 -s BA.BIN BA.ASM MJ_SND_BA
+smps2asm -v 1 -s BB.BIN BB.ASM MJ_SND_BB
+smps2asm -v 1 -s BC.BIN BC.ASM MJ_SND_BC
+smps2asm -v 1 -s BD.BIN BD.ASM MJ_SND_BD
+smps2asm -v 1 -s BE.BIN BE.ASM MJ_SND_BE
+smps2asm -v 1 -s BF.BIN BF.ASM MJ_SND_BF
+echo	Converting Section 3...
+smps2asm -v 1 -s C0.BIN C0.ASM MJ_SND_C0
+smps2asm -v 1 -s C1.BIN C1.ASM MJ_SND_C1
+smps2asm -v 1 -s C2.BIN C2.ASM MJ_SND_C2
+smps2asm -v 1 -s C3.BIN C3.ASM MJ_SND_C3
+smps2asm -v 1 -s C4.BIN C4.ASM MJ_SND_C4
+smps2asm -v 1 -s C5.BIN C5.ASM MJ_SND_C5
+smps2asm -v 1 -s C6.BIN C6.ASM MJ_SND_C6
+smps2asm -v 1 -s C7.BIN C7.ASM MJ_SND_C7
+smps2asm -v 1 -s C8.BIN C8.ASM MJ_SND_C8
+smps2asm -v 1 -s C9.BIN C9.ASM MJ_SND_C9
+smps2asm -v 1 -s CA.BIN CA.ASM MJ_SND_CA
+smps2asm -v 1 -s CB.BIN CB.ASM MJ_SND_CB
+smps2asm -v 1 -s CC.BIN CC.ASM MJ_SND_CC
+smps2asm -v 1 -s CD.BIN CD.ASM MJ_SND_CD
+smps2asm -v 1 -s CE.BIN CE.ASM MJ_SND_CE
+smps2asm -v 1 -s CF.BIN CF.ASM MJ_SND_CF
+echo	Converting Section 4...
+smps2asm -v 1 -s D0.BIN D0.ASM MJ_SND_D0
+echo	Completed!
+pause
